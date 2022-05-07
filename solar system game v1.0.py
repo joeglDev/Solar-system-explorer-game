0ab8c93761abd647dabeb809a70b973aa4f798e7
Joe Gilbert (He/Him) <104070284+joeglDev@users.noreply.github.com>
2022-05-07 11:43:53 +0100

    Add files via upload

---
+++ solar system game v1.0.py
@@ -0,0 +1,8 @@
+#USE CASES
+
+#1. Procedural object genration of planets (attributes), moons (attributes), solar systems 
+#create object contructor, give it a list of possible inputs, randomise selection
+#2. Storage of these genertated objecyts for later
+#3. Ability for user to visit diff objects in a text based game
+#4. stretch - format text into a nice GUI
+
